a2cfe2eb23421d258d1ecea8996a3c8f4d6b7aee
codevkim <codev.kim@gmail.com>
2024-06-09 23:32:21 +0900

    update

--- animalname_back/src/main/java/kr/co/animalname/WebConfig.java
+++ animalname_back/src/main/java/kr/co/animalname/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
 
 
-        
 	
 	//Controller마다 CrossOrigin("*")설정을 안해도 됨
 	//프로젝트 전체에 한 번에 적용하는 것이다. => rest 서버를 사용하려면 본 작업을 진행해야 한다.
